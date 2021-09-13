bafc44a5435b28d3714db98907b1f67d9ec5f1a4
elcinp <elcinpars1@gmail.com>
2021-09-13 22:29:59 +0300

    redux-clarusshop

---
+++ src/redux/actions/actions.js
@@ -0,0 +1,27 @@
+import {SELL_LAPTOP,SELL_PHONE,SELL_WATCH,SELL_COMP} from "../types/types"
+
+export const sellLaptop = (number=1) => {
+    return{
+        type:SELL_LAPTOP,
+        payload:number
+    };
+};
+
+export const sellPhone = (number=1) => {
+    return{
+        type:SELL_PHONE,
+        payload:number
+    }
+}
+
+export const sellWatch = () => {
+    return{
+        type:SELL_WATCH,
+    }
+}
+
+export const SellWatch = () => {
+    return{
+        type:SELL_COMP,
+    }
+}
---
+++ src/redux/reducers/laptopReducer.js
@@ -0,0 +1,20 @@
+import {SELL_LAPTOP} from '../types/types'
+
+const initialstate = {
+    numberOfLaptops: 100,
+}
+
+const laptopReducer = (state=initialstate,action) => {
+    switch (action.type) {
+        case SELL_LAPTOP:
+            return{
+                ...state,
+                numberOfLaptops: state.numberOfLaptops-action.payload
+            }
+            
+    
+        default:
+            return state;
+    }
+}
+export default laptopReducer;
